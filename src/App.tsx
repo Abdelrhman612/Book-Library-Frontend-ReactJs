2eeaea0508a461910adf6377770323a9d24fa93b
Abdelrhman <abdo612200320@gmail.com>
2025-05-14 13:45:39 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/Login";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;
