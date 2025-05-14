bb11d98862d2d729d7bd5d0f0070f199fc31057f
Abdelrhman <abdo612200320@gmail.com>
2025-05-14 13:46:03 +0300

    update authService.tsx

--- src/Service/authService.tsx
+++ src/Service/authService.tsx
@@ -1,7 +1,5 @@
 import axios from "axios";
 import type { LoginInterFace } from "./AuthInterface";
-
 const API_URL = "http://localhost:5000/api/v1/auth/sign-in";
-
 export const login = ({ email, password }: LoginInterFace) =>
   axios.post(API_URL, { email, password });
