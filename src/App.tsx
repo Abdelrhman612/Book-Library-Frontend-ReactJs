9ba9b8d2479073c30c3f679d3ada324d2bad42a9
Abdelrhman <abdo612200320@gmail.com>
2025-05-14 22:33:27 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
+import VerifyCode from "./pages/VerifyCode/VerifyCode";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signUp" element={<SignUp />}></Route>
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/verify-code" element={<VerifyCode />}></Route>
       </Routes>
     </BrowserRouter>
   );
