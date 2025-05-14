868d3cc68bcb86b255275dd67981f202e7ee67dd
Abdelrhman <abdo612200320@gmail.com>
2025-05-14 15:59:55 +0300

    feat: create ResetPassword.tsx

---
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -0,0 +1,11 @@
+import ResetPasswordComponents from "../../components/resetPassword/ResetPasswordComponents";
+
+const ResetPassword = () => {
+  return (
+    <div>
+      <ResetPasswordComponents />
+    </div>
+  );
+};
+
+export default ResetPassword;
