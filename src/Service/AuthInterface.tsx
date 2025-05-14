576f8ae22c8ff58e75b0244968864c681c5a4a45
Abdelrhman <abdo612200320@gmail.com>
2025-05-14 16:00:10 +0300

    update AuthInterface.tsx

--- src/Service/AuthInterface.tsx
+++ src/Service/AuthInterface.tsx
@@ -1,4 +1,9 @@
-export interface LoginInterFace {
+export interface Login {
+  email: string;
+  password: string;
+}
+export interface SignUp {
+  name: string;
   email: string;
   password: string;
 }
