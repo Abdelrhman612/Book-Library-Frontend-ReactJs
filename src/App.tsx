d09a0a48d1e5e24239f0d610675516e9ee010b92
Abdelrhman <abdo612200320@gmail.com>
2025-05-14 15:59:15 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import "./App.css";
+import "./index.css";
 import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import ResetPassword from "./pages/ResetPassword/ResetPassword";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/signUp" element={<SignUp />}></Route>
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
