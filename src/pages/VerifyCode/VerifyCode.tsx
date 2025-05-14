f8a62508c6a5346a49bfb2a080356ab13adaa0b0
Abdelrhman <abdo612200320@gmail.com>
2025-05-14 22:33:57 +0300

    feat: create VerifyCode.tsx

---
+++ src/pages/VerifyCode/VerifyCode.tsx
@@ -0,0 +1,7 @@
+import VerifyCodeComponents from "../../components/Auth/VerifyCode/VerifyCodeComponents";
+
+const VerifyCode = () => {
+  return <VerifyCodeComponents />;
+};
+
+export default VerifyCode;
