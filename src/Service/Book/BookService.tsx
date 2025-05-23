f7ad1be5ea591ced6cfb37788826d38fb5fa3415
Abdelrhman <abdo612200320@gmail.com>
2025-05-23 05:49:00 +0300

    update BookService.tsx

--- src/Service/Book/BookService.tsx
+++ src/Service/Book/BookService.tsx
@@ -1,2 +1,15 @@
+import axios from "axios";
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const API_URL_Books = `${API_BASE_URL}/api/v1/book`;
+const API_URL_Books = `${API_BASE_URL}/book`;
+
+export const bookService = () => {
+  const token = localStorage.getItem("token");
+  const getAllBooks = () => {
+    return axios.get(API_URL_Books, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  };
+
+  return { getAllBooks };
+};
