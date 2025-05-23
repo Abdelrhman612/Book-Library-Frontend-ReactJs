e3a86d9bc19b733f7821c874a51f737914de4c40
Abdelrhman <abdo612200320@gmail.com>
2025-05-23 05:48:05 +0300

    update BookList.tsx

--- src/components/Book/BookList.tsx
+++ src/components/Book/BookList.tsx
@@ -1,3 +1,40 @@
-export const BookList = () => {
-  return <div>BookList</div>;
+import { useEffect, useState } from "react";
+import BookCard from "./BookCard";
+import { bookService } from "../../Service/Book/BookService";
+
+type Book = {
+  id: string | number;
+  title: string;
+  author: string;
+  category: string;
+  description: string;
+  image: string;
 };
+
+export const BookList = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  useEffect(() => {
+    async function fetchBooks() {
+      try {
+        const res = await bookService().getAllBooks();
+        setBooks(res.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchBooks();
+  }, []);
+
+  return (
+    <div className="container mt-5">
+      <h2 className="mb-4">قائمة الكتب</h2>
+      <div className="row">
+        {books.map((book) => (
+          <BookCard key={book.id} {...book} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BookList;
