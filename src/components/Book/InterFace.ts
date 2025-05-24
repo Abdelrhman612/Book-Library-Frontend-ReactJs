c0466bd262ab589c1bae895d7d09fb09854fe867
Abdelrhman <abdo612200320@gmail.com>
2025-05-24 20:52:56 +0300

    feat: create InterFace.ts

---
+++ src/components/Book/InterFace.ts
@@ -0,0 +1,15 @@
+export type Book = {
+  id: string | number;
+  title: string;
+  author: string;
+  category: string;
+  description: string;
+  image: string;
+};
+export type BookCardProps = {
+  title: string;
+  author: string;
+  category: string;
+  description: string;
+  image: string;
+};
