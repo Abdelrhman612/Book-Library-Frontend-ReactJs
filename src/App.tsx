e59276353ec9a4a187f2a510759b32b7e1091b18
Abdelrhman <abdo612200320@gmail.com>
2025-06-01 03:09:53 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import "./index.css";
-import Book from "./pages/Book/Book";
 import LoginPage from "./components/Auth/Login/LoginPage";
 import ChangePasswordPage from "./components/Auth/ChangePassword/ChangePasswordPage";
 import ResetPasswordPage from "./components/Auth/ResetPaasword/ResetPasswordPage";
 import SignUpPage from "./components/Auth/SignUp/SignUpPage";
 import VerifyCodePage from "./components/Auth/VerifyCode/VerifyCodePage";
+import BookList from "./components/Book/BookList";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         <Route path="/reset-password" element={<ResetPasswordPage />} />
         <Route path="/verify-code" element={<VerifyCodePage />}></Route>
         <Route path="/change-password" element={<ChangePasswordPage />}></Route>
-        <Route path="/homePage" element={<Book />}></Route>
+        <Route path="/homePage" element={<BookList />}></Route>
       </Routes>
     </BrowserRouter>
   );
