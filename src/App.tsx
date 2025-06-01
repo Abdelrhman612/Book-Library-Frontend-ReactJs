77d0f4e50c9f4f71864dfad3b7700b6a4aab46c0
Abdelrhman <abdo612200320@gmail.com>
2025-06-01 03:05:55 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import "./index.css";
-import ResetPassword from "./pages/Auth/ResetPassword";
 import Book from "./pages/Book/Book";
-import SignUp from "./pages/Auth/SignUp";
-import VerifyCode from "./pages/Auth/VerifyCode";
 import LoginPage from "./components/Auth/Login/LoginPage";
 import ChangePasswordPage from "./components/Auth/ChangePassword/ChangePasswordPage";
+import ResetPasswordPage from "./components/Auth/ResetPaasword/ResetPasswordPage";
+import SignUpPage from "./components/Auth/SignUp/SignUpPage";
+import VerifyCodePage from "./components/Auth/VerifyCode/VerifyCodePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/signUp" element={<SignUp />}></Route>
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/verify-code" element={<VerifyCode />}></Route>
+        <Route path="/signUp" element={<SignUpPage />}></Route>
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/verify-code" element={<VerifyCodePage />}></Route>
         <Route path="/change-password" element={<ChangePasswordPage />}></Route>
         <Route path="/homePage" element={<Book />}></Route>
       </Routes>
