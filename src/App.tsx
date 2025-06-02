6fc207999f5775e2709944f089fe77ff601fe828
Abdelrhman <abdo612200320@gmail.com>
2025-06-03 01:23:40 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,17 @@ import ResetPasswordPage from "./components/Auth/ResetPaasword/ResetPasswordPage
 import SignUpPage from "./components/Auth/SignUp/SignUpPage";
 import VerifyCodePage from "./components/Auth/VerifyCode/VerifyCodePage";
 import BookList from "./components/Book/BookList";
-import ReviewList from "./components/Review/ReviewList";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/" element={<LoginPage />}></Route>
         <Route path="/signUp" element={<SignUpPage />}></Route>
         <Route path="/reset-password" element={<ResetPasswordPage />} />
         <Route path="/verify-code" element={<VerifyCodePage />}></Route>
         <Route path="/change-password" element={<ChangePasswordPage />}></Route>
         <Route path="/homePage" element={<BookList />}></Route>
-        <Route path="/review" element={<ReviewList />}></Route>
       </Routes>
     </BrowserRouter>
   );
