22e8737690d2d55522b478ed8a26dbcd6402fcae
Abdelrhman <abdo612200320@gmail.com>
2025-06-06 02:05:02 +0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+dotenv.config();
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(process.env.PORT),
+    port: Number(process.env.PORT) || 3000,
   },
 });
